15a933d851468f77e1b19d8db2d07f15f453ee77
bourbonracer <bourbonracer@gmail.com>
2020-09-26 19:58:07 -0700

    Created Y axis label

--- assets/js/app.js
+++ assets/js/app.js
@@ -95,5 +95,13 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
       .on("mouseout", function(data, index) {
           toolTip.hide(data);
       });
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Smokers (%)");
 
-})
+});
