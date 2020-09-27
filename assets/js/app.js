c1c038834772f9841e94a95836023e2763ea9cdd
bourbonracer <bourbonracer@gmail.com>
2020-09-26 17:05:19 -0700

    Import data, parse data, cast as numbers, created scale functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+// Create an SVG wrapper, append an SVG group to hold our chart, and shift the chart by left and top margins.
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
@@ -22,3 +22,22 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 var textGrp = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    // Parse Data/Cast as numbers
+    healthData.forEach(function(data) {
+      data.age = +data.age;
+      data.smokes = +data.smokes;
+    });
+
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+      .domain(d3.max(healthData, d=> d.age))
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain(d3.max(healthData, d => d.smokes))
+      .range([height, 0]);
+})
